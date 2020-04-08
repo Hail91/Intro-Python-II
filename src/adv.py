f5396fb60393a5db944a47e1519117e76f79cd7a
Hail91 <aarongillies88@gmail.com>
2020-04-08 18:54:51 -0400

    Finished day 1 MVP, starting to set up day 2 stuff

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,26 @@
 from room import Room
+from player import Player
 
 # I'm here!
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", []),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", []),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", []),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", []),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", []),
 }
 
 
@@ -37,8 +38,45 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
-
 # Make a new player object that is currently in the 'outside' room.
+name = input("Please enter your player name: ")   # Take input to get player's name
+player = Player(name, room['outside']) # Create new player by calling the Player class to instantiate a new player object.
+                                       # Set player's name to the name inputted, start them in the room "outside".
+print('You are currently in', player.location.name)
+print(player.location.description)
+
+while True:
+    cmd = input("Please enter a direction you would like to travel in.... [n] [e] [s] [w], or press q to quit the game: ")
+
+    if cmd == 'n':
+        if player.location.n_to is not None:
+            player.location = player.location.n_to
+            print(f'You are now in {player.location.name}, {player.location.description}')
+        else:
+            print(f'You cannot go that way!')
+    elif cmd == 'e':
+        if player.location.e_to is not None:
+            player.location = player.location.e_to
+            print(f'You are now in {player.location.name}, {player.location.description}')
+        else: 
+            print(f'You cannot go that way!')
+    elif cmd == 's':
+        if player.location.s_to is not None:
+            player.location = player.location.s_to
+            print(f'You are now in {player.location.name}, {player.location.description}')
+        else:
+            print(f'You cannot go that way!')
+    elif cmd == 'w':
+        if player.location.w_to is not None:
+            player.location = player.location.w_to
+            print(f'You are now in {player.location.name}, {player.location.description}')
+        else:
+            print(f'You cannot go that way!')
+    elif cmd == 'q':
+        exit()
+    else:
+        print('You must choose either [n] [s] [e] [w]!')
+
 
 # Write a loop that:
 #
@@ -50,3 +88,4 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+ 
